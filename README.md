e90390477e37573153e2a86e669a76b31b16b214
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-11-15 13:08:07 +1300

    tidy

---
+++ README.md
@@ -0,0 +1,4 @@
+# Crosscoders
+
+Replication of some of Anthropic's recent research update on Crosscoders: [Sparse Crosscoders for Cross-Layer Features and Model Diffing
+](https://transformer-circuits.pub/2024/crosscoders/index.html)
--- main.py
+++ main.py
@@ -1,57 +1,61 @@
 import torch as t
 from torch import nn
 
-
 class CrossCoder(nn.Module):
     def __init__(self, n_layers: int, layer_dim: int, hidden_dim: int):
         super(CrossCoder, self).__init__()
 
-        self.enc_weights_LHF = nn.Parameter(t.randn(n_layers, hidden_dim, layer_dim))
+        self.enc_weights_LHD = nn.Parameter(t.randn(n_layers, hidden_dim, layer_dim))
         self.enc_bias_H = nn.Parameter(t.zeros(hidden_dim))
 
-        self.dec_weights_LFH = nn.Parameter(t.randn(n_layers, layer_dim, hidden_dim))
-        self.dec_bias_LF = nn.Parameter(t.zeros(n_layers, layer_dim))
+        self.dec_weights_LDH = nn.Parameter(t.randn(n_layers, layer_dim, hidden_dim))
+        self.dec_bias_LD = nn.Parameter(t.zeros(n_layers, layer_dim))
 
-    def encode(self, activation_NLF: t.Tensor) -> t.Tensor:
+    def encode(self, activation_NLD: t.Tensor) -> t.Tensor:
         """
-        activation_NLF: (batch_size, n_layers, layer_dim)
+        activation_NLD: (batch_size, n_layers, layer_dim)
         """
-        hidden_NLH = t.einsum("nlf,lhf->nlh", activation_NLF, self.enc_weights_LHF)
+        hidden_NLH = t.einsum("nld,lhd->nlh", activation_NLD, self.enc_weights_LHD)
         hidden_NH = t.einsum("nlh->nh", hidden_NLH) + self.enc_bias_H
         return t.relu(hidden_NH)
 
     def decode(self, hidden_NH: t.Tensor) -> t.Tensor:
         """
-        hidden_NH: (batch_size, n_layers)
+        hidden_NH: (batch_size, hidden_dim)
         """
-        activation_NLF = t.einsum("nh,lfh->nlf", hidden_NH, self.dec_weights_LFH)
-        activation_NLF += self.dec_bias_LF
-        return activation_NLF
+        activation_NLD = t.einsum("nh,ldh->nld", hidden_NH, self.dec_weights_LDH)
+        activation_NLD += self.dec_bias_LD
+        return activation_NLD
 
-    def forward(self, activation_NLF):
-        hidden_NH = self.encode(activation_NLF)
-        reconstructed_NLF = self.decode(hidden_NH)
-        return reconstructed_NLF
+    def forward(self, activation_NLD: t.Tensor) -> t.Tensor:
+        """
+        activation_NLD: (batch_size, n_layers, layer_dim)
+        """
+        hidden_NH = self.encode(activation_NLD)
+        reconstructed_NLD = self.decode(hidden_NH)
+        return reconstructed_NLD
 
     def sparsity_loss(self, hidden_NH: t.Tensor) -> t.Tensor:
-        dec_weights_norm_LH = self.dec_weights_LFH.norm(dim=1)
+        dec_weights_norm_LH = self.dec_weights_LDH.norm(dim=1)
         dec_weights_norm_H = t.einsum("lh->h", dec_weights_norm_LH)
         return t.einsum("nh,h->n", hidden_NH, dec_weights_norm_H).mean()
 
     def reconstruction_loss(
-        self, activation_NLF: t.Tensor, target_NLF: t.Tensor
+        self, activation_NLD: t.Tensor, target_NLD: t.Tensor
     ) -> t.Tensor:
-        x_NL = (activation_NLF - target_NLF).norm(dim=-1).square()
+        x_NL = (activation_NLD - target_NLD).norm(dim=-1).square()
         x_N = t.einsum("nl->n", x_NL)
         return x_N.mean()
 
-    def forward_train(self, activation_NLF: t.Tensor) -> tuple[t.Tensor, t.Tensor]:
-        hidden_NH = self.encode(activation_NLF)
-        reconstructed_NLF = self.decode(hidden_NH)
-        reconstruction_loss = self.reconstruction_loss(reconstructed_NLF, activation_NLF)
+    def forward_train(self, activation_NLD: t.Tensor) -> tuple[t.Tensor, t.Tensor]:
+        hidden_NH = self.encode(activation_NLD)
+        reconstructed_NLD = self.decode(hidden_NH)
+        reconstruction_loss = self.reconstruction_loss(
+            reconstructed_NLD, activation_NLD
+        )
         sparsity_loss = self.sparsity_loss(hidden_NH)
         loss = reconstruction_loss + sparsity_loss
-        return reconstructed_NLF, loss
+        return reconstructed_NLD, loss
 
 
 if __name__ == "__main__":
@@ -60,10 +64,10 @@ if __name__ == "__main__":
     layer_dim = 16
     hidden_dim = 256
     model = CrossCoder(n_layers=n_layers, layer_dim=layer_dim, hidden_dim=hidden_dim)
-    x_NLF = t.randn(batch_size, n_layers, layer_dim)
-    y_NLF = model.forward(x_NLF)
-    assert y_NLF.shape == x_NLF.shape
+    x_NLD = t.randn(batch_size, n_layers, layer_dim)
+    y_NLD = model.forward(x_NLD)
+    assert y_NLD.shape == x_NLD.shape
 
-    y_NLF, loss = model.forward_train(x_NLF)
-    assert y_NLF.shape == x_NLF.shape
+    y_NLD, loss = model.forward_train(x_NLD)
+    assert y_NLD.shape == x_NLD.shape
     assert loss.shape == ()
